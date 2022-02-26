308637810bc353e165e4bfeaeb65923d2e48f5be
patha <patha@DESKTOP-AMGVC49>
2022-02-26 23:35:51 +0530

    assistedproject1

---
+++ stringtoprimitives/src/stringtoprimitives/firstproject.java
@@ -0,0 +1,16 @@
+package stringtoprimitives;
+
+public class firstproject {
+	
+	public static void main(String args[]) {
+		
+		int a = Integer.parseInt("10");
+		
+		long l = Long.parseLong("40");
+		
+		float f = Float.parseFloat("40.5");
+		
+		double d = Double.parseDouble("80.5");
+	}
+
+}
