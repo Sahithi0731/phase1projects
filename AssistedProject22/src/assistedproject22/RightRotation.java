9765fde7fe758af22be78ad4a91c15be54264a78
patha <patha@DESKTOP-AMGVC49>
2022-02-28 10:16:52 +0530

    assistedproject22

---
+++ AssistedProject22/src/assistedproject22/RightRotation.java
@@ -0,0 +1,24 @@
+package assistedproject22;
+import java.util.Arrays;
+public class RightRotation {
+	private static void RightRotate(int[] inputArray, int n)
+	{
+		System.out.println("Input Array Before Rotation");
+		System.out.println(Arrays.toString(inputArray));
+		int temp;
+		for(int i =1; i<=n;i++)
+		{
+			temp = inputArray[inputArray.length-1];
+			for(int j=inputArray.length-1;j>0; j--)
+			{
+				inputArray[j]=inputArray[j-1];
+			}
+			inputArray[0]=temp;
+		}
+		System.out.println("Input Array After Right Rotation By "+n+" Positions:");
+		System.out.println(Arrays.toString(inputArray));
+	}
+	public static void main(String[] args){
+		RightRotate(new int[] {1, 2, 3, 4, 5, 6, 7}, 2);
+	}
+}
